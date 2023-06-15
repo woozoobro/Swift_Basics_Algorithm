5367f0e0c074989b4c8802defbd511d9d190c59e
woozoobro <woojusm@gmail.com>
2023-06-16 00:35:04 +0900

    coding test

--- ForTest.playground/Contents.swift
+++ ForTest.playground/Contents.swift
@@ -1,16 +1,38 @@
 import Foundation
 
-func solution(_ arr:[Int]) -> [Int] {
-    var stk = [Int]()
+func solution(_ a: Int, _ b: Int, _ c: Int, _ d: Int) -> Int {
+    let dice = [a, b, c, d]
+    var counts = [Int: Int]()
+
+    for i in dice {
+        counts[i, default: 0] += 1
+    }
     
-    for i in 0..<arr.count {
-        if stk.isEmpty || stk.last! < arr[i] {
-            stk.append(arr[i])
-        } else if stk.last! >= arr[i] {
-            stk.removeLast()
+    if counts.count == 1 {
+        let p = dice[0]
+        return 1111 * p
+    } else if counts.count == 4 {
+        return dice.min()!
+    } else if counts.count == 3 {
+        let p = counts.first(where: { $0.value == 2 })!.key
+        let q = counts.first(where: {$0.value == 1})!.key
+        let r = dice.filter { $0 != p  && $0 != q }.first!
+        return q * r
+    } else {
+        if counts.values.contains(2) {
+            let p = counts.first(where: {$0.value == 2})!.key
+            let q = dice.filter{ $0 != p }.first!
+            return (p+q) * abs(p-q)
+        } else {
+            let p = counts.first(where: {$0.value == 3})!.key
+            let q = dice.filter{ $0 != p}.first!
+            return (10*p + q) * (10*p + q)
         }
     }
-    return stk
 }
 
-solution([1, 4, 2, 5, 3])
+solution(2, 2, 2, 2)
+solution(4, 1, 4, 4)
+solution(6, 3, 3, 6)
+solution(2, 5, 2, 6)
+solution(6, 4, 2, 5)
---
+++ Udemy_Arcade_Algorithm/Graphs/What are Graphs?.md
@@ -0,0 +1,17 @@
+Very important subject for Senior Engineers
+
+사실 그래프는 Binary Tree에서 이미 접했었던 거임!!
+
+그래프는 탑에서 바텀으로 방향성이 있음
+
+> G = ( V, E )
+
+A graph G is an ordered pair of a set of vertices V and a set of edges E.
+
+Edges에는 두 가지 종류
+directed 랑 undirected 이렇게 두 가지
+
+
+### Weighted Graph
+여기 옵시디언에 그래프 기능 있잖음. 거리 막 밸류에 따라서 멀어지게 표현 가능하잖음. 그거임
+
